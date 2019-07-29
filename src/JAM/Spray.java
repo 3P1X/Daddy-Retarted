05a6a596f0113dfe4aa035be5700b52f00c677a4
TroddenSpade <parsasam1378@gmail.com>
2019-07-29 15:14:11 +0430

    Update Spray.java

--- src/JAM/Spray.java
+++ src/JAM/Spray.java
@@ -2,6 +2,6 @@ package JAM;
 
 public class Spray extends Item{
     public Spray(){
-        super(System.getProperty("user.dir")+"/assets/"+"newspaper.png",800,860,485);
+        super(System.getProperty("user.dir")+"/assets/"+"spray.png",800,860,485);
     }
 }
